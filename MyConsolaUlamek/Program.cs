5a34f4124ecd7e55aa7ed3084e6ccb070daf5193
szymon.spiewak <szymon.spiewak@NY-13>
2024-02-27 14:29:09 +0100

    Add project files.

---
+++ MyConsolaUlamek/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsolaUlamek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ulamek a = new Ulamek(5, 4);
+            Ulamek b = new Ulamek(7, 4);
+            Ulamek c =a * b;
+
+            Console.Write(c.ToString());
+            //Ulamek c = Ulamek.Polowa;
+            //Ulamek d = Ulamek.cwiarc;
+
+            //Console.WriteLine((a+b).ToString());
+            //Console.WriteLine((c/b).ToString());
+
+            //Ulamek[] tablicaUlamkow = new Ulamek(10);
+
+        }
+    }
+}
---
+++ MyConsolaUlamek/Ulamek.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsolaUlamek
+{
+    internal class Ulamek
+    {
+        public int licznik;
+        public int mianownik;
+        public Ulamek(int in_licznik, int in_mianownik) {
+            
+            if(in_mianownik == 0)
+            {
+                throw new Exception("Nie dzielimy przez 0");
+            }
+            licznik = in_licznik;
+            mianownik = in_mianownik;
+        }
+
+        public void Uprosc()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return licznik.ToString()+"/"+mianownik.ToString();
+        }
+        public static Ulamek operator*(Ulamek valueA, Ulamek valueB)
+        {
+            Ulamek wynik = new Ulamek(valueA.licznik * valueB.licznik,valueB.mianownik  * valueB.mianownik);
+            wynik.Uprosc();
+            return wynik;
+        }
+
+        public static Ulamek operator+(Ulamek valueA, Ulamek valueB)
+        {
+            
+        }
+    }
+}
