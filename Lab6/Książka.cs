5a8f33b634efc2ecfb070e84520d25a46dede93d
HellHood <szymon.spiewak@microsoft.wsei.edu.pl>
2024-04-09 14:32:39 +0200

    Linq

---
+++ Lab6/Książka.cs
@@ -0,0 +1,76 @@
+﻿using Lab6;
+using System;
+using System.Collections.Generic;
+
+    public class Książka
+    {
+            public string Tytuł { get; set; }
+            public string Autor { get; set; }
+            public int RokWydania { get; set; }
+            public string Gatunek { get; set; }
+            public double Ocena { get; set; }
+
+
+
+
+
+            public static void Print(IEnumerable<Książka> książki)
+            {
+                foreach (var książka in książki)
+                {
+                    Console.WriteLine($"Tytuł: {książka.Tytuł}, Autor: {książka.Autor}, Rok wydania: {książka.RokWydania}, Gatunek: {książka.Gatunek}, Ocena: {książka.Ocena}");
+                }
+            }
+
+
+
+            public static List<Książka> GenerujKsiążki()
+            {
+                List<Książka> książki = new List<Książka>();
+
+
+
+                książki.Add(new Książka { Tytuł = "Harry Potter and the Philosopher's Stone", Autor = "J.K. Rowling", RokWydania = 1997, Gatunek = "Fantasy", Ocena = 4.5 });
+                książki.Add(new Książka { Tytuł = "To Kill a Mockingbird", Autor = "Harper Lee", RokWydania = 1960, Gatunek = "Dramat", Ocena = 4.8 });
+                książki.Add(new Książka { Tytuł = "1984", Autor = "George Orwell", RokWydania = 1949, Gatunek = "Science Fiction", Ocena = 4.6 });
+                książki.Add(new Książka { Tytuł = "The Catcher in the Rye", Autor = "J.D. Salinger", RokWydania = 1951, Gatunek = "Dramat", Ocena = 4.3 });
+                książki.Add(new Książka { Tytuł = "The Great Gatsby", Autor = "F. Scott Fitzgerald", RokWydania = 1925, Gatunek = "Dramat", Ocena = 4.7 });
+                książki.Add(new Książka { Tytuł = "Pride and Prejudice", Autor = "Jane Austen", RokWydania = 1813, Gatunek = "Romans", Ocena = 4.8 });
+                książki.Add(new Książka { Tytuł = "The Hobbit", Autor = "J.R.R. Tolkien", RokWydania = 1937, Gatunek = "Fantasy", Ocena = 4.9 });
+                książki.Add(new Książka { Tytuł = "The Lord of the Rings", Autor = "J.R.R. Tolkien", RokWydania = 1954, Gatunek = "Fantasy", Ocena = 4.9 });
+                książki.Add(new Książka { Tytuł = "The Chronicles of Narnia", Autor = "C.S. Lewis", RokWydania = 1950, Gatunek = "Fantasy", Ocena = 4.6 });
+                książki.Add(new Książka { Tytuł = "To the Lighthouse", Autor = "Virginia Woolf", RokWydania = 1927, Gatunek = "Fikcja literacka", Ocena = 4.2 });
+                książki.Add(new Książka { Tytuł = "Moby-Dick", Autor = "Herman Melville", RokWydania = 1851, Gatunek = "Przygodowy", Ocena = 4.4 });
+                książki.Add(new Książka { Tytuł = "Crime and Punishment", Autor = "Fyodor Dostoevsky", RokWydania = 1866, Gatunek = "Dramat", Ocena = 4.7 });
+                książki.Add(new Książka { Tytuł = "The Picture of Dorian Gray", Autor = "Oscar Wilde", RokWydania = 1890, Gatunek = "Fantasy", Ocena = 4.5 });
+                książki.Add(new Książka { Tytuł = "Wuthering Heights", Autor = "Emily Bronte", RokWydania = 1847, Gatunek = "Dramat", Ocena = 4.6 });
+                książki.Add(new Książka { Tytuł = "Anna Karenina", Autor = "Leo Tolstoy", RokWydania = 1877, Gatunek = "Dramat", Ocena = 4.7 });
+                książki.Add(new Książka { Tytuł = "Gone with the Wind", Autor = "Margaret Mitchell", RokWydania = 1936, Gatunek = "Dramat", Ocena = 4.6 });
+                książki.Add(new Książka { Tytuł = "Don Quixote", Autor = "Miguel de Cervantes", RokWydania = 1605, Gatunek = "Przygodowy", Ocena = 4.5 });
+                książki.Add(new Książka { Tytuł = "The Count of Monte Cristo", Autor = "Alexandre Dumas", RokWydania = 1844, Gatunek = "Przygodowy", Ocena = 4.7 });
+                książki.Add(new Książka { Tytuł = "Frankenstein", Autor = "Mary Shelley", RokWydania = 1818, Gatunek = "Science Fiction", Ocena = 4.3 });
+                książki.Add(new Książka { Tytuł = "Dracula", Autor = "Bram Stoker", RokWydania = 1897, Gatunek = "Horror", Ocena = 4.4 });
+                książki.Add(new Książka { Tytuł = "Les Misérables", Autor = "Victor Hugo", RokWydania = 1862, Gatunek = "Dramat", Ocena = 4.7 });
+                książki.Add(new Książka { Tytuł = "Jane Eyre", Autor = "Charlotte Bronte", RokWydania = 1847, Gatunek = "Romans", Ocena = 4.8 });
+                książki.Add(new Książka { Tytuł = "War and Peace", Autor = "Leo Tolstoy", RokWydania = 1869, Gatunek = "Dramat", Ocena = 4.7 });
+                książki.Add(new Książka { Tytuł = "The Odyssey", Autor = "Homer", RokWydania = -800, Gatunek = "Epos", Ocena = 4.6 });
+                książki.Add(new Książka { Tytuł = "The Iliad", Autor = "Homer", RokWydania = -800, Gatunek = "Epos", Ocena = 4.6 });
+                książki.Add(new Książka { Tytuł = "The Divine Comedy", Autor = "Dante Alighieri", RokWydania = 1320, Gatunek = "Poemat epicki", Ocena = 4.7 });
+                książki.Add(new Książka { Tytuł = "The Canterbury Tales", Autor = "Geoffrey Chaucer", RokWydania = 1400, Gatunek = "Opowieści", Ocena = 4.5 });
+                książki.Add(new Książka { Tytuł = "The Adventures of Huckleberry Finn", Autor = "Mark Twain", RokWydania = 1884, Gatunek = "Przygodowy", Ocena = 4.4 });
+                książki.Add(new Książka { Tytuł = "Alice's Adventures in Wonderland", Autor = "Lewis Carroll", RokWydania = 1865, Gatunek = "Fantasy", Ocena = 4.6 });
+                książki.Add(new Książka { Tytuł = "The War of the Worlds", Autor = "H.G. Wells", RokWydania = 1898, Gatunek = "Science Fiction", Ocena = 4.3 });
+                książki.Add(new Książka { Tytuł = "The Adventures of Sherlock Holmes", Autor = "Arthur Conan Doyle", RokWydania = 1892, Gatunek = "Detektywistyczny", Ocena = 4.7 });
+                książki.Add(new Książka { Tytuł = "Little Women", Autor = "Louisa May Alcott", RokWydania = 1868, Gatunek = "Dramat", Ocena = 4.5 });
+                książki.Add(new Książka { Tytuł = "Brave New World", Autor = "Aldous Huxley", RokWydania = 1932, Gatunek = "Science Fiction", Ocena = 4.4 });
+                książki.Add(new Książka { Tytuł = "One Hundred Years of Solitude", Autor = "Gabriel García Márquez", RokWydania = 1967, Gatunek = "Fantasy", Ocena = 4.6 });
+                książki.Add(new Książka { Tytuł = "Catch-22", Autor = "Joseph Heller", RokWydania = 1961, Gatunek = "Satyra", Ocena = 4.3 });
+                książki.Add(new Książka { Tytuł = "The Grapes of Wrath", Autor = "John Steinbeck", RokWydania = 1939, Gatunek = "Dramat", Ocena = 4.7 });
+                książki.Add(new Książka { Tytuł = "The Bell Jar", Autor = "Sylvia Plath", RokWydania = 1963, Gatunek = "Autobiografia", Ocena = 4.5 });
+
+
+
+                return książki;
+            }
+        }
+    
---
+++ Lab6/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Książka> książki = Książka.GenerujKsiążki();
+            Książka.Print(książki);
+            Console.WriteLine("\r\n");
+
+
+
+            //Znajdź wszystkie książki wydane po roku 2000
+            Console.WriteLine("Znajdź wszystkie książki wydane po roku 2000:");
+            var ksiazkiNowe = książki.Where(p => p.RokWydania > 2000);
+            Książka.Print(ksiazkiNowe);
+            Console.WriteLine("\r\n");
+
+
+
+            //Znajdź średnią ocenę wszystkich książek:
+            Console.WriteLine("Znajdź średnią ocenę wszystkich książek:");
+            var sredniaOcena = książki.Average(p => p.Ocena);
+            Console.WriteLine(sredniaOcena);
+            Console.WriteLine("\r\n");
+
+
+
+            // Znajdź 3 najlepiej ocenione książki
+            Console.WriteLine("Znajdź 3 najlepiej ocenione książki:");
+            var najlepsze = książki.OrderByDescending(p => p.Ocena).Take(3);
+            Książka.Print(najlepsze);
+            Console.WriteLine("\r\n");
+
+
+            //Posortuj książki według roku wydania
+            Console.WriteLine("Posortuj książki według roku wydania:");
+            var książkiOrderYear = książki.OrderBy(p => p.RokWydania);
+            Książka.Print(książkiOrderYear);
+            Console.WriteLine("\r\n");
+
+
+            //Znajdź najstarszą i najnowszą książkę w bazie.
+            Console.WriteLine("Znajdź najstarszą i najnowszą książkę w bazie:");
+            var książkaStara = książki.OrderBy(p => p.RokWydania).Take(1);
+            var książkaNowa = książki.OrderByDescending(p => p.RokWydania).Take(1);
+            Książka.Print(książkaStara);
+            Książka.Print(książkaNowa);
+            Console.WriteLine("\r\n");
+
+
+            //Znajdź wszystkie unikalne gatunki książek obecne w bazie.
+            Console.WriteLine("Znajdź wszystkie unikalne gatunki książek obecne w bazie:");
+            var unikalne = książki.GroupBy(p => p.Gatunek);
+            Console.WriteLine("\r\n");
+
+            
+
+
+            //Znajdź wszystkie książki napisane przez danego autora
+            Console.WriteLine("Znajdź wszystkie książki napisane przez danego autora");
+            var książkiAutora = książki.Where(p => p.Autor == "Emily Bronte");
+            Książka.Print(książkiAutora);
+            Console.WriteLine("\r\n");
+
+
+            Thread.Sleep(1000000);
+        }
+
+    }
+    
+}
+
